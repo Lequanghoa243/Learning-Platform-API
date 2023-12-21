f67ee715b572f715c5198257432e1a033aee91b4
Lequanghoa243 <115691400+Lequanghoa243@users.noreply.github.com>
2023-12-21 14:38:19 +0700

    update

--- app/configs/jwtToken.js
+++ app/configs/jwtToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const generateToken = (id) => {
-    return jwt.sign({id}, process.env.jwt_secret ,{expiresIn: "3d"});
+    return jwt.sign({id}, "mysecret" ,{expiresIn: "3d"});
 };
 
 module.exports = {generateToken};
